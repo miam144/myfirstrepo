c40a84116435af2e72b4a3f50aae29cd533a3030
miam144 <33631606+miam144@users.noreply.github.com>
2017-11-14 07:52:04 -0500

    Add files via upload

---
+++ personality_mm.py
@@ -0,0 +1,21 @@
+name = "Mia"
+myresteraunts = "Rynaldys"
+myfavfoods = ["High school special", "chicken fingers", "tacos in a bag"]
+sport = "tennis"
+tvshow = "Gossip Girl"
+movie = "Safe Haven"
+subject = "Spanish"
+color = "light blue"
+wosrtsubject = "History"
+
+print("Welcome, " + name + "!")
+print("Have you visited " + state + " or " + city + " recently?")
+print("oh, you haven't? Probably watching too much " + tvshow)
+print("How many times have you watched" + movie)
+
+elif sport == "Tennis":
+    print("I love to play doubles with my friend Sienna!")
+elif subject == "Spanish"
+    print("My favorite subject is Spanish in school")
+
+
